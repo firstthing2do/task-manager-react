75d8cddf8ba045f47683ad5bd4965888445d5751
firstthing2do <atgpro100@gmail.com>
2024-07-12 14:20:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import React  from "react";
+import {useState} from 'react'
+import Header from "./components/Header"
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+const App =  () => {
+  const[showAddTask, setShowAddTask] = useState(false)
+  const[tasks, setTasks] = useState( [
+])
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
 }
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 100) + 1
+  
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ): (
+        'No tasks to show'
+      )}
+      </div>
+  )
+}
+
+
+// using class
+// class App extends React.Component {
+//   render(){
+//     return <h1>HEllo from clas</h1>
+//   }
+// }
 
 export default App;
